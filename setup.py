61d16a083e6043a13160ec2caea6edaca9f0d4d7
Florent Lebreton <florentlebreton@free.fr>
2016-10-28 09:07:28 +0200

    Update CHANGELOG and bump to 0.5

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 setup(
     name='django-multi-email-field',
-    version='0.4',
+    version='0.5',
     author='Florent Lebreton',
     author_email='florent.lebreton@makina-corpus.com',
     url='https://github.com/fle/django-multi-email-field',
-    download_url="https://github.com/fle/django-multi-email-field/tarball/0.4",
+    download_url="https://github.com/fle/django-multi-email-field/tarball/0.5",
     description="Provides a model field and a form field to manage list of e-mails",
     long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
         open(os.path.join(here, 'CHANGES')).read(),
@@ -25,13 +25,9 @@ setup(
         'Intended Audience :: Developers',
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Development Status :: 4 - Beta',
-        'Programming Language :: Python :: 2.7',
+        'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
