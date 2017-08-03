e9fa03e464d65e15d74f07ba8ddcf1effe57a822
Leo Antunes <leo@costela.net>
2017-08-03 11:07:07 +0200

    remove download_url (unsupported since PEP 470)
    PyPI used to be able to scrape download_url and pip would also make use of it as an "external repository". This has been unsupported since PEP 470 was implemented (~2015-09).
    As of now, releases to pypi have to be uploaded directly. This commit just makes this more explicit.
    
    fixes #5

--- setup.py
+++ setup.py
@@ -9,11 +9,10 @@ setup(
     author='Florent Lebreton',
     author_email='florent.lebreton@makina-corpus.com',
     url='https://github.com/fle/django-multi-email-field',
-    download_url="https://github.com/fle/django-multi-email-field/tarball/0.5",
     description="Provides a model field and a form field to manage list of e-mails",
     long_description=open(os.path.join(here, 'README.rst')).read() + '\n\n' +
         open(os.path.join(here, 'CHANGES')).read(),
-    license='LPGL, see LICENSE file.',
+    license='LGPL, see LICENSE file.',
     install_requires=['Django'],
     packages=find_packages(),
     include_package_data=True,
