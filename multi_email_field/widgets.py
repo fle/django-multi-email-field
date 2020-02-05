59278ed0d29c1afe326dfbad128a189d1e9a6e31
Sebastien Corbin <sebastien.corbin@makina-corpus.com>
2020-02-05 16:32:35 +0100

    Drop six dependency, remove Django<1.11 and Python 3.5 tests

--- multi_email_field/widgets.py
+++ multi_email_field/widgets.py
@@ -1,21 +1,25 @@
-import six
-
 from django.forms.widgets import Textarea
 from django.core.exceptions import ValidationError
 from django.core import validators
 
 MULTI_EMAIL_FIELD_EMPTY_VALUES = validators.EMPTY_VALUES + ('[]', )
 
+try:
+    from django.utils import six
+
+    string_types = six.string_types
+except ImportError:
+    string_types = str
+
 
 class MultiEmailWidget(Textarea):
-
     is_hidden = False
 
     def prep_value(self, value):
         """ Prepare value before effectively render widget """
         if value in MULTI_EMAIL_FIELD_EMPTY_VALUES:
             return ""
-        elif isinstance(value, six.string_types):
+        elif isinstance(value, string_types):
             return value
         elif isinstance(value, list):
             return "\n".join(value)
