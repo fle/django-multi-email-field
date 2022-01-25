99a27ccdad465ff13a08cdfdd6b2612f817d32fc
Mohammad Ahtasham ul Hassan <60315450+aht007@users.noreply.github.com>
2022-01-25 15:12:07 +0500

    fix: fix RemovedInDjango40 error

--- multi_email_field/forms.py
+++ multi_email_field/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from multi_email_field.widgets import MultiEmailWidget
 
